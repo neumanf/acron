606af4c70bf5f0f13c4aae0c33c7771e41e81234
neumanf <fabricionewman@protonmail.com>
2020-04-10 16:44:45 -0300

    Updates

---
+++ precache-manifest.7662e0fe63f62b5fbda097066d7afc2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fddcce3babb8735a4871e8ceb15a7339",
+    "url": "/acron/index.html"
+  },
+  {
+    "revision": "8e9fa28a8bacdf3bdc79",
+    "url": "/acron/static/css/main.3a24a090.chunk.css"
+  },
+  {
+    "revision": "95c12d55f546d52594e0",
+    "url": "/acron/static/js/2.a5c64a5c.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/acron/static/js/2.a5c64a5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e9fa28a8bacdf3bdc79",
+    "url": "/acron/static/js/main.a7b0f062.chunk.js"
+  },
+  {
+    "revision": "1d9239b5f0d64ed4f43b",
+    "url": "/acron/static/js/runtime-main.099129ac.js"
+  },
+  {
+    "revision": "6b54249239cc42e785cfec9434246f97",
+    "url": "/acron/static/media/bg-pattern.6b542492.svg"
+  }
+]);
